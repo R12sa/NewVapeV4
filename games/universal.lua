3c9266b6ef6d92b585a4ab3476639cf48b35d23a
R12sa <144062428+R12sa@users.noreply.github.com>
2026-01-12 17:12:45 -0800

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -14,7 +14,7 @@ end
 local function downloadFile(path, func)
 	if not isfile(path) then
 		local suc, res = pcall(function()
-			return game:HttpGet('https://raw.githubusercontent.com/s4v7/SavyV4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
+			return game:HttpGet('https://raw.githubusercontent.com/R12sa/NewVapeV4/'..readfile('newvape/profiles/commit.txt')..'/'..select(1, path:gsub('newvape/', '')), true)
 		end)
 		if not suc or res == '404: Not Found' then
 			error(res)
